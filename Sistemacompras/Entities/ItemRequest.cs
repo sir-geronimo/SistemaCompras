c2121db9a6de66178929351be7ff433ccccb5c53
EJGamer21 <engersky12@gmail.com>
2020-02-15 13:39:13 -0400

    Added missing entities

---
+++ Sistemacompras/Entities/ItemRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistemacompras.Contracts;
+
+namespace Sistemacompras.Entities
+{
+    class ItemRequest : IAuditableEntity
+    {
+        public int Id { get; set; }
+        public int RequestedBy { get; set; }
+        public virtual Employee Employee { get; set; }
+        public DateTime Date { get; set; }
+        public int ItemId { get; set; }
+        public virtual Item Item { get; set; }
+        public int Quantity { get; set; }
+        public int UnitId { get; set; }
+        public virtual Unit Unit { get; set; }
+        public int StatusId { get; set; }
+        public virtual Status Status { get; set; }
+
+        #region Interface
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        #endregion
+    }
+}
---
+++ Sistemacompras/Entities/PurchaseOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistemacompras.Contracts;
+
+namespace Sistemacompras.Entities
+{
+    class PurchaseOrder : IAuditableEntity
+    {
+        public int Id { get; set; }
+        public int ItemRequestId { get; set; }
+        public virtual ItemRequest ItemRequest { get; set; }
+        public DateTime Date { get; set; }
+        public int StatusId { get; set; }
+        public virtual Status Status { get; set; }
+        public int ItemId { get; set; }
+        public virtual Item Item { get; set; }
+        public int Quantity { get; set; }
+        public int UnitId { get; set; }
+        public virtual Unit Unit { get; set; }
+        public int BrandId { get; set; }
+        public virtual Brand Brand { get; set; }
+        public double Price { get; set; }
+
+        #region Interface
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        #endregion
+    }
+}

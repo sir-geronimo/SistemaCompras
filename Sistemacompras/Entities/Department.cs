d2888803bb05482e67674896ecc55debabf06b35
EJGamer21 <engersky12@gmail.com>
2020-02-15 13:26:51 -0400

    Created Entities

---
+++ Sistemacompras/Entities/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistemacompras.Contracts;
+
+namespace Sistemacompras.Entities
+{
+    class Department : IAuditableEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int StatusId { get; set; }
+        public virtual Status Status { get; set; }
+
+        #region Interface
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        #endregion
+    }
+}
---
+++ Sistemacompras/Entities/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistemacompras.Contracts;
+
+namespace Sistemacompras.Entities
+{
+    class Item : IAuditableEntity
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int BrandId { get; set; }
+        public virtual Brand Brand { get; set; }
+        public int UnitId { get; set; }
+        public virtual Unit Unit { get; set; }
+        public int Stock { get; set; }
+        public int StatusId { get; set; }
+        public virtual Status Status { get; set; }
+
+        #region Interface
+        public DateTime CreatedDate { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public int UpdatedBy { get; set; }
+        #endregion
+    }
+}

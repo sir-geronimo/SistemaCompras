c1b7f9cae55c6c36f55325ce56534d49aff30251
Enger Jimenez <ejimenezr21@gmail.com>
2020-03-22 16:32:52 -0400

    Develop (#5)
    * Added login
    
    * added vs files
    
    * updated gitignore
    
    * Updated gitignore
    
    * Updated gitignore
    
    * Updated gitignore
    
    * Removed folders
    
    * ?
    
    * Deleted duplicted initial migration
    
    * Removed generic repository
    
    Co-authored-by: Enger Jiménez <ejimenez@oriontek.do>

--- Sistemacompras/Forms/Home.cs
+++ Sistemacompras/Forms/Home.cs
@@ -645,7 +645,7 @@ namespace Sistemacompras
         {
             RefreshBtn_Click(sender, e);
         }
-
+        
         private void LogoutBtn_Click(object sender, EventArgs e)
         {
             Close();
